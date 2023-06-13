745df0f56c1b2cc0843d415d83f55f1e633187d2
BekirLee <nihadbekirli8@gmail.com>
2023-06-13 23:31:48 +0400

    changes for music progect

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
+const container = document.querySelector(".container");
+const image = document.querySelector("#music_img");
+const title = document.querySelector(".music_details #title");
+const singer = document.querySelector(".music_details #singer");
+const prev = document.querySelector(".controllers #prev");
+const play = document.querySelector(".controllers #play");
+const next = document.querySelector(".controllers #next");
+
+
+
+
+
+
+
+
 
 let player = new MusicPlayer(musicList);
 
 let music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.previous();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
 console.log(music.getName());
 
 player.next();

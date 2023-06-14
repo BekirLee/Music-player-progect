279ca65e2aca1b1ee136eee46700bb0b5d4d35f7
BekirLee <nihadbekirli8@gmail.com>
2023-06-14 10:30:17 +0400

    music progect working

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
 const container = document.querySelector(".container");
 const image = document.querySelector("#music_img");
-const title = document.querySelector(".music_details #title");
-const singer = document.querySelector(".music_details #singer");
-const prev = document.querySelector(".controllers #prev");
-const play = document.querySelector(".controllers #play");
-const next = document.querySelector(".controllers #next");
+const title = document.querySelector("#music_details #title");
+const singer = document.querySelector("#music_details #singer");
+const prev = document.querySelector("#controllers #prev");
+const play = document.querySelector("#controllers #play");
+const next = document.querySelector("#controllers #next");
+
+const player = new MusicPlayer(musicList);
+
+
+
+window.addEventListener("load", () => {
+    let music = player.getMusic();
+    displayMusic(music);
+})
+
+function displayMusic(music) {
+    title.innerText = music.getName();
+    singer.innerText = music.singer;
+    image.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
+};
+
+play.addEventListener("click", () => {
+    const isMusicPLay = container.classList.contains("playing");
+    isMusicPLay ? pauseMusic() : playMusic();
+});
+
+function prevMusic() {
+    player.previous();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+prev.addEventListener("click", () => {
+    prevMusic();
+})
 
 
 
 
 
+function playMusic() {
+    container.classList.add("playing");
+    audio.play();
+    play.classList = ("fa-solid fa-pause");
+}
+
+function pauseMusic() {
+    container.classList.remove("playing");
+    audio.pause();
+    play.classList = ("fa-solid fa-play");
+
+
+}
 
 
 
-
-let player = new MusicPlayer(musicList);
-
-let music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
